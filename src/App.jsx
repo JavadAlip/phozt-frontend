fe59240e5a5d17582e768f01f42d72cd04406740
JavadAlip <javadalipp911@gmail.com>
2025-10-13 14:35:34 +0530

    test

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Dashboard from "./pages/Dashboard";
+// import Dashboard from "./pages/Dashboard";
+import Leads from "./pages/LeadsManagement";
 
 export default function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Dashboard />} />
+        {/* <Route path="/" element={<Dashboard />} /> */}
+        <Route path="/" element={<Leads />} />
       </Routes>
     </Router>
   );
